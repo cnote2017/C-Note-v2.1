0d05f57ef5c2eb617595aa4d727aaac6249e7177
jcaferjr <jcaferjr@yahoo.com>
2017-03-08 14:44:08 -0600

    Update

--- compile-db.sh
+++ compile-db.sh
@@ -11,16 +11,16 @@ if [ ! -d "db-4.8.30.NC" ]; then
   echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef  db-4.8.30.NC.tar.gz' | sha256sum -c
   # -> db-4.8.30.NC.tar.gz: OK
   tar -xzvf db-4.8.30.NC.tar.gz
+
+  # Build the library and install to our prefix
+  cd db-4.8.30.NC/build_unix/
+  #  Note: Do a static build so that it can be embedded into the exectuable, instead of having to find a .so at runtime
+  ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
+  make install
+  cd $BITCOIN_ROOT
 fi
 
-# Build the library and install to our prefix
-cd db-4.8.30.NC/build_unix/
-#  Note: Do a static build so that it can be embedded into the exectuable, instead of having to find a .so at runtime
-../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
-make install
-
 # Configure Bitcoin Core to use our own-built instance of BDB
-cd $BITCOIN_ROOT
 ./configure LDFLAGS="-L${BDB_PREFIX}/lib/" CPPFLAGS="-I${BDB_PREFIX}/include/"
 
 make
